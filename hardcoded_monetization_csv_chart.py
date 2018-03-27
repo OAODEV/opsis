6e8cfb9d3479ea3245177e4b41907fbec1186d00
Jesse Miller <jesse.miller@adops.com>
2018-03-27 13:53:11 -0500

    debug

--- hardcoded_monetization_csv_chart.py
+++ hardcoded_monetization_csv_chart.py
@@ -3,8 +3,8 @@ from io import StringIO
 
 def monetization_csv_chart(data, report_name, display):
     print(data)
-    f = StringIO(data)
-    reader = csv.reader(f, delimiter=',', newline='\r\n')
+    f = StringIO(data, newline='\r\n')
+    reader = csv.reader(f, delimiter=',')
     rows = list(reader)
     print(rows)
     rows[0] = ["Date", "Demand Source", "Impressions", "Estimated Revenue", "eCPM"]
