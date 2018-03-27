a6e289cbb11e1350f4155ef08071aa258d982265
Jesse Miller <jesse.miller@adops.com>
2018-03-27 16:15:59 -0500

    debug

--- hardcoded_monetization_csv_chart.py
+++ hardcoded_monetization_csv_chart.py
@@ -3,13 +3,12 @@ from io import StringIO
 import json
 
 def monetization_csv_chart(data, report_name, display):
-    print(data)
+    print("pre", data)
     data = data.replace(
         'date,demand_source,ad_server_impressions,total_cpm_and_cpc_revenue,total_ecpm',
         'Date,Demand Source,Impressions,Estimated Revenue,eCPM',
     )
-    print(data)
+    print("post", data)
     data = json.dumps(data)
-    print(data)
+    print("json", data)
     return data
-
