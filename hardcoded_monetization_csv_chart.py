a78aa99d0d10fdbd2884a0e6d18436aa5703f851
Jesse Miller <jesse.miller@adops.com>
2018-03-27 16:13:28 -0500

    debug

--- hardcoded_monetization_csv_chart.py
+++ hardcoded_monetization_csv_chart.py
@@ -6,7 +6,7 @@ def monetization_csv_chart(data, report_name, display):
     print(data)
     data = data.replace(
         'date,demand_source,ad_server_impressions,total_cpm_and_cpc_revenue,total_ecpm',
-        'Date,"Demand Source",Impressions,"Estimated Revenue",eCPM',
+        'Date,Demand Source,Impressions,Estimated Revenue,eCPM',
     )
     print(data)
     data = json.dumps(data)
