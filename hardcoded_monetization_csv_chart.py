bc0ee9e67261174c08ee9064112526de415aab60
Jesse Miller <jesse.miller@adops.com>
2018-03-27 16:53:26 -0500

    rewrite csv header row

--- hardcoded_monetization_csv_chart.py
+++ hardcoded_monetization_csv_chart.py
@@ -3,12 +3,8 @@ from io import StringIO
 import json
 
 def monetization_csv_chart(data, report_name, display):
-#    print("pre", data)
     data = data.replace(
         'date,demand_source,ad_server_impressions,total_cpm_and_cpc_revenue,total_ecpm',
         'Date,Demand Source,Impressions,Estimated Revenue,eCPM',
     )
-#    print("post", data)
-#    data = json.dumps(data)
-    print("replaced", data)
     return data
